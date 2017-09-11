01fc793df173b897503f4bea6c1d0af89f99845b
Clint <clintmad21@hotmail.com>
2017-09-11 13:43:19 -0600

    Need CSS now

---
+++ quote-controller.js
@@ -0,0 +1,36 @@
+function QuoteController() {
+    var quoteService = new QuoteService();
+    $("#button").on("click", function newQuote() {
+        quoteService.getQuote().then(function (data) {
+            console.log(data);
+
+            var quoteTitle = quoteService.extract(data[0].title);
+            var quoteContent = quoteService.extract(data[0].content);
+            var wholeContent = quoteContent + " -" + quoteTitle;
+            var template = "";
+            var url = "https://twitter.com/intent/tweet?text=" + encodeURIComponent(quoteContent);
+            var tUrl = "https://www.tumblr.com/widgets/share/tool?posttype=quote&tags=quotes&caption=" +
+                encodeURIComponent(quoteTitle) + '&content=' + encodeURIComponent(quoteContent) +
+                '&canonicalUrl=https://www.tumblr.com';
+
+            template += `
+                <h2>${data[0].content}</h2>
+                <h3> -${data[0].title}</h3>
+                <button class="tweet btn btn-primary"><i class="fa fa-twitter"></i></button>
+                <button class="tumblr btn"><i class="fa fa-tumblr"></i></button>
+                `;
+
+            $("#quote").empty();
+            $("#quote").append($(template));
+
+            $('.tweet').click(function () {
+                window.open(url);
+            });
+            $('.tumblr').click(function () {
+                window.open(tUrl);
+            })
+        });
+
+    });
+}
+QuoteController();
---
+++ quote-service.js
@@ -0,0 +1,30 @@
+function QuoteService() {
+	var url = "https://bcw-getter.herokuapp.com/?url=";
+	var url2 =
+		"https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1";
+	var apiUrl = url + encodeURIComponent(url2);
+
+	this.getQuote = function () {
+		return new Promise(function (resolve, reject) {
+			$.get(apiUrl).then(
+				function (data) {
+					var result = JSON.parse(data);
+					resolve(result);
+				},
+				function (error) {
+					reject(error);
+				}
+			);
+		});
+	};
+
+	this.extract = function (string) {
+		var span = $("<span/>");
+		string = span
+			.html(string)
+			.text()
+			.replace(/[\u2018\u2019]/g, "'")
+			.replace(/[\u201C\u201D]/g, '"');
+		return string;
+	};
+}
